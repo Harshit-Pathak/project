4349db649d60a0dcbf75fa3266341c15398049a4
Harshit Pathak <harshitpathak29@gmail.com>
2021-02-03 21:13:44 +0530

    trial commit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,7 +12,7 @@ import {
 function Navigation() {
   return (
        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a className="navbar-brand" href="#">DataStruc and Algos</a>
+  <a className="navbar-brand" href="#">DS and ALGO</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
